7663a27756b37e449df28a3b30786a2d14ea4245
buumbuum6789 <vuduchong209305@gmail.com>
2018-10-25 15:10:05 +0700

    init

---
+++ precache-manifest.8a7c9b6e776e9c1d618f2dcba39a4f7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "86f7e2cfe05fe3ae6bf8",
+    "url": "/js/chunk-vendors.ae147928.js"
+  },
+  {
+    "revision": "dc5eeb9d21e571c59869",
+    "url": "/js/app.4206c861.js"
+  },
+  {
+    "revision": "b7b277c856bc2e0e83d476e6303124da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bd79053718d0907ffca1cc8f3919ac",
+    "url": "/img/avatar.f7bd7905.png"
+  },
+  {
+    "revision": "86f7e2cfe05fe3ae6bf8",
+    "url": "/css/chunk-vendors.2b9fc83b.css"
+  },
+  {
+    "revision": "dc5eeb9d21e571c59869",
+    "url": "/css/app.8880b117.css"
+  }
+];
